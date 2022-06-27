63471b6eb06485778e016b66d1b944c4eff7b101
vinicius.silva <daftwarz@gmail.com>
2022-06-27 10:27:21 -0300

    correcao

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
@@ -6,7 +6,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { catchError, Observable } from 'rxjs';
 import { Cliente } from '../domain/cliente';
 import { ClienteModel } from '../model/cliente-model';
 
@@ -19,6 +19,7 @@ export class ClienteComponent implements OnInit {
   list: Cliente[] = [];
 
   form: FormGroup = this.formBuilder.group({
+    id: new FormControl(null),
     nome: new FormControl(null, [Validators.required, Validators.minLength(2)]),
     cpf: new FormControl(null, [Validators.required, Validators.minLength(11)]),
     email: new FormControl(null, [Validators.required, Validators.email]),
@@ -28,6 +29,9 @@ export class ClienteComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.carregarTabela();
+  }
+  private carregarTabela() {
     this.get().subscribe((domains: Cliente[]) => {
       if (domains) {
         this.list = domains;
@@ -36,17 +40,50 @@ export class ClienteComponent implements OnInit {
   }
 
   cadastrar(): void {
+    const id = this.form.controls['id'].value;
     const cliente: ClienteModel = this.form.getRawValue();
-    this.post(cliente).subscribe((domain: Cliente) => {
-      if (domain.id) {
-        this.list.push(domain);
-      }
+    if (id) {
+      this.put(id, cliente).subscribe((domain: Cliente) => {
+        if (domain.id) {
+          this.carregarTabela();
+          this.form.reset();
+        }
+      });
+    } else {
+      this.post(cliente).subscribe((domain: Cliente) => {
+        if (domain.id) {
+          this.list.push(domain);
+          this.form.reset();
+        }
+      });
+    }
+  }
+
+  editar(cliente: Cliente): void {
+    this.form.controls['id'].setValue(cliente.id);
+    this.form.controls['nome'].setValue(cliente.id);
+    this.form.controls['cpf'].setValue(cliente.documento);
+    this.form.controls['email'].setValue(cliente.email);
+    this.form.controls['aniver'].setValue(cliente.aniver);
+  }
+  deletar(id: string): void {
+    alert('Voce quer deletar isso ??');
+    this.delete(id).subscribe((domain: Cliente) => {
+      this.carregarTabela();
     });
   }
 
+  private delete(id: string): Observable<Cliente> {
+    const url = 'http://localhost:8080/cliente/remover/' + id;
+    return this.http.delete<Cliente>(url);
+  }
   private post(model: ClienteModel): Observable<Cliente> {
     const url = 'http://localhost:8080/cliente/cadastrar';
     return this.http.post<Cliente>(url, model);
+  }
+  private put(id: string, model: ClienteModel): Observable<Cliente> {
+    const url = 'http://localhost:8080/cliente/alterar/' + id;
+    return this.http.put<Cliente>(url, model);
   }
 
   private get(): Observable<Cliente[]> {
